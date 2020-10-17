0067fd873fca3226b9dbc7a52fc60a8245d1dc35
Daniel Rios <danielseguro@gmail.com>
2020-10-17 13:38:31 -0500

    Versión5

--- FinalLogicaProgramacion.java
+++ FinalLogicaProgramacion.java
@@ -1,12 +1,35 @@
 
 package finallogicaprogramacion;
 
-
 public class FinalLogicaProgramacion {
+        public static void acertaste() {
+                System.out.println("   \\  :  /       \\  :  /       \\ :  /       \\  :  /");
+                System.out.println("`. __/ \\__ .' `. __/ \\__ .'`. __/ \\__ .' `. __/ \\__ .'");
+                System.out.println("_ _\\     /_ _ _ _\\     /_ __ _\\     /_ _ _ _\\     /_ _");
+                System.out.println("   /_   _\\       /_   _\\      /_   _\\       /_   _\\");
+                System.out.println(" .'  \\ /  `.   .'  \\ /  `.  .'  \\ /  `.   .'  \\ /  `.");
+                System.out.println("   /  |  \\       /  :  \\      /  :  \\       /  |  \\");
+                System.out.println("      |                                        |");
+                System.out.println("   \\  |  /                                  \\  |  /");
+                System.out.println("`. __/ \\__ .'                            `. __/ \\__ .'");
+                System.out.println("_ _\\     /_ _          Acertaste         _ _\\     /_ _");
+                System.out.println("   /_   _\\                                  /_   _\\");
+                System.out.println(" .'  \\ /  `.                              .'  \\ /  `.");
+                System.out.println("   /  |  \\                                  /  |  \\");
+                System.out.println("      |                                        |");
+                System.out.println("   \\  |  /       \\  :  /      \\  :  /       \\  |  /");
+                System.out.println("`. __/ \\__ .' `. __/ \\__ .'`. __/ \\__ .' `. __/ \\__ .'");
+                System.out.println("_ _\\     /_ _ _ _\\     /_ __ _\\     /_ _ _ _\\     /_ _");
+                System.out.println("   /_   _\\       /_   _\\      /_   _\\       /_   _\\");
+                System.out.println(" .'  \\ /  `.   .'  \\ /  `.  .'  \\ /  `.   .'  \\ /  `.");
+                System.out.println("   /  :  \\       /  :  \\      /  :  \\      /  :  \\");
+        }
+
+        public static void main(String[] args) {
+
+        }
+
+}
 
   
-    public static void main(String[] args) {
-     
-    }
-    
-}
+

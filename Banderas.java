615adfa738620c088d18368485c01b7b3c48768b
Daniel Rios <danielseguro@gmail.com>
2020-10-17 20:37:36 -0500

    Se agrega bandera de Colombia

--- Banderas.java
+++ Banderas.java
@@ -25,6 +25,31 @@ public class Banderas{
                 System.out.println();   
         }
     }
+    public static void  colombia(){
+        String[] archivox = ConsoleFile.read("recursos/info_banderas.csv");
+
+            for(int n=22;n<40;n++){
+                String[] archivo = archivox[n].split(";"); // Archivox es el documento sin comas
+                for(int i=0;i<archivo.length;i++){
+                    switch (archivo[i]){
+                        case "1":
+                            System.out.print(ConsoleColors.RED_BACKGROUND+"   ");
+                            break;
+                        case "2":
+                            System.out.print(ConsoleColors.BLUE_BACKGROUND+"   ");
+                            break;
+                        case "4":
+                            System.out.print(ConsoleColors.YELLOW_BACKGROUND+"   ");
+                            break;
+                        default:
+                            System.out.print("   ");
+                }
+                System.out.print(ConsoleColors.RESET);
+             
+            }
+                System.out.println();   
+        }
+    }
 
     
 }

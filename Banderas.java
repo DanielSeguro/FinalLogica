907ded06380c945f2ad340492e5657a347ca9c56
Daniel Rios <danielseguro@gmail.com>
2020-10-18 10:18:28 -0500

    Se añaden banderas de Peru y Polonia

--- Banderas.java
+++ Banderas.java
@@ -1,55 +1,102 @@
-public class Banderas{
+public class Banderas {
 
-    public static void  gran_Breta(){
+    public static void gran_Breta() {
         String[] archivox = ConsoleFile.read("recursos/info_banderas.csv");
 
-            for(int n=0;n<20;n++){
-                String[] archivo = archivox[n].split(";"); // Archivox es el documento sin comas
-                for(int i=0;i<archivo.length;i++){
-                    switch (archivo[i]){
-                        case "1":
-                            System.out.print(ConsoleColors.RED_BACKGROUND+"   ");
-                            break;
-                        case "2":
-                            System.out.print(ConsoleColors.BLUE_BACKGROUND+"   ");
-                            break;
-                        case "3":
-                            System.out.print(ConsoleColors.WHITE_BACKGROUND+"   ");
-                            break;
-                        default:
-                            System.out.print("   ");
+        for (int n = 0; n < 20; n++) {
+            String[] archivo = archivox[n].split(";"); // Archivox es el documento sin comas
+            for (int i = 0; i < archivo.length; i++) {
+                switch (archivo[i]) {
+                    case "1":
+                        System.out.print(ConsoleColors.RED_BACKGROUND + "   ");
+                        break;
+                    case "2":
+                        System.out.print(ConsoleColors.BLUE_BACKGROUND + "   ");
+                        break;
+                    case "3":
+                        System.out.print(ConsoleColors.WHITE_BACKGROUND + "   ");
+                        break;
+                    default:
+                        System.out.print("   ");
                 }
                 System.out.print(ConsoleColors.RESET);
-             
-            }
-                System.out.println();   
-        }
-    }
-    public static void  colombia(){
-        String[] archivox = ConsoleFile.read("recursos/info_banderas.csv");
 
-            for(int n=22;n<40;n++){
-                String[] archivo = archivox[n].split(";"); // Archivox es el documento sin comas
-                for(int i=0;i<archivo.length;i++){
-                    switch (archivo[i]){
-                        case "1":
-                            System.out.print(ConsoleColors.RED_BACKGROUND+"   ");
-                            break;
-                        case "2":
-                            System.out.print(ConsoleColors.BLUE_BACKGROUND+"   ");
-                            break;
-                        case "4":
-                            System.out.print(ConsoleColors.YELLOW_BACKGROUND+"   ");
-                            break;
-                        default:
-                            System.out.print("   ");
-                }
-                System.out.print(ConsoleColors.RESET);
-             
             }
-                System.out.println();   
+            System.out.println();
         }
     }
 
-    
+    public static void colombia() {
+        String[] archivox = ConsoleFile.read("recursos/info_banderas.csv");
+
+        for (int n = 22; n < 40; n++) {
+            String[] archivo = archivox[n].split(";"); // Archivox es el documento sin comas
+            for (int i = 0; i < archivo.length; i++) {
+                switch (archivo[i]) {
+                    case "1":
+                        System.out.print(ConsoleColors.RED_BACKGROUND + "   ");
+                        break;
+                    case "2":
+                        System.out.print(ConsoleColors.BLUE_BACKGROUND + "   ");
+                        break;
+                    case "4":
+                        System.out.print(ConsoleColors.YELLOW_BACKGROUND + "   ");
+                        break;
+                    default:
+                        System.out.print("   ");
+                }
+                System.out.print(ConsoleColors.RESET);
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void peru() {
+        String[] archivox = ConsoleFile.read("recursos/info_banderas.csv");
+
+        for (int n = 42; n < 60; n++) {
+            String[] archivo = archivox[n].split(";"); // Archivox es el documento sin comas
+            for (int i = 0; i < archivo.length; i++) {
+                switch (archivo[i]) {
+                    case "1":
+                        System.out.print(ConsoleColors.RED_BACKGROUND + "   ");
+                        break;
+                    case "3":
+                        System.out.print(ConsoleColors.WHITE_BACKGROUND + "   ");
+                        break;
+
+                    default:
+                        System.out.print("   ");
+                }
+                System.out.print(ConsoleColors.RESET);
+
+            }
+            System.out.println();
+        }
+    }
+    public static void polonia() {
+        String[] archivox = ConsoleFile.read("recursos/info_banderas.csv");
+
+        for (int n = 62; n < 80; n++) {
+            String[] archivo = archivox[n].split(";"); // Archivox es el documento sin comas
+            for (int i = 0; i < archivo.length; i++) {
+                switch (archivo[i]) {
+                    case "1":
+                        System.out.print(ConsoleColors.RED_BACKGROUND + "   ");
+                        break;
+                    case "3":
+                        System.out.print(ConsoleColors.WHITE_BACKGROUND + "   ");
+                        break;
+
+                    default:
+                        System.out.print("   ");
+                }
+                System.out.print(ConsoleColors.RESET);
+
+            }
+            System.out.println();
+        }
+    }
+
 }

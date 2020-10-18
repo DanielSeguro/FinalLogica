7b9fbd49c66bc0abe82b82d62eab03cceca626b0
Daniel Rios <danielseguro@gmail.com>
2020-10-18 14:47:20 -0500

    Se agregan 3 preguntas al archvio ascii

--- Ascii.java
+++ Ascii.java
@@ -38,4 +38,90 @@ public class Ascii{
                 System.out.println(" X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X");
                 System.out.println("(_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_)");
         }
+        public static void menuprincipal() {
+                System.out.println(".---..   ..---. .--. .--.   .--.    .    .   ..--. .---..--.     .    .-. ");
+                System.out.println("    ||   ||    :    :    :  |   )  / \\   |\\  ||   :|    |   )   / \\  (   )");
+                System.out.println("    ||   ||--- | --.|    |  |--:  /___\\  | \\ ||   ||--- |--'   /___\\  `-. ");
+                System.out.println("    ;:   ;|    :   |:    ;  |   )/     \\ |  \\||   ;|    |  \\  /     \\(   )");
+                System.out.println("`--'  `-' '---' `--' `--'   '--''       `'   ''--' '---''   `'       ``-' ");        
+                System.out.println("                         ,---.           ,---.");
+                System.out.println("                        / /\"`.\\.--\"\"\"--./,'\"\\ \\");
+                System.out.println("                        \\ \\    _       _    / /");
+                System.out.println("                         `./  / __   __ \\  \\,'");
+                System.out.println("                          /    /_O)_(_O\\    \\");
+                System.out.println("                          |  .-'  ___  `-.  |");
+                System.out.println("                       .--|       \\_/       |--.");
+                System.out.println("                     ,'    \\   \\   |   /   /    `.");
+                System.out.println("                    /       `.  `--^--'  ,'       \\");
+                System.out.println("                 .-\"\"\"\"\"-.    `--.___.--'     .-\"\"\"\"\"-.");
+                System.out.println("    .-----------/         \\------------------/         \\--------------.");
+                System.out.println("    | .---------\\         /----------------- \\         /------------. |");
+                System.out.println("    | |          `-`--`--'                    `--'--'-'             | |");
+                System.out.println("    | |                                                             | |");
+                System.out.println("    | |                                                             | |");
+                System.out.println("    | |                                                             | |");
+                System.out.println("    | |              1. Cuestionario de los paises.                 | |");
+                System.out.println("    | |              2. Identifica el pais.                         | |");
+                System.out.println("    | |              3. Mostrar bandera del pais que desees.        | |");
+                System.out.println("    | |              4. Salir del programa.                         | |");
+                System.out.println("    | |                                                             | |");
+                System.out.println("    | |                                                             | |");
+                System.out.println("    | |Desarrollado por:                                            | |");
+                System.out.println("    | |-Juan Pablo Montoya Orozco                                   | |");
+                System.out.println("    | |-Daniel Seguro Rios                                          | |");
+                System.out.println("    | |_____________________________________________________________| |");
+                System.out.println("    |_________________________________________________________________|");
+                System.out.println("                       )__________|__|__________(");
+                System.out.println("                      |            ||            |");
+                System.out.println("                      |____________||____________|");
+                System.out.println("                        ),-----.(      ),-----.(");
+                System.out.println("                      ,'   ==.   \\    /  .==    `.");
+                System.out.println("                     /            )  (            \\");
+                System.out.println("                     `==========='    `==========='");
+        }
+        public static void capital() {
+                System.out.println("   .-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-.");
+                System.out.println(".-'---      - ---     --     ---   - -    ---'-.");
+                System.out.println(" )                                            (");
+                System.out.println("(                                              )");
+                System.out.println(" )                                            (");
+                System.out.println("(              Cual es su capital?             )");
+                System.out.println(" )                                            (");
+                System.out.println("(                                              )");
+                System.out.println(" )                                            (");
+                System.out.println("(___       _       _       _       _        ___)");
+                System.out.println("    (_____) (_____) (_____) (_____)  (_____)");
+                System.out.println("            (_____) (_____) (_____)");
+                System.out.println("                    (_____)                  ");
+        }
+        public static void idioma() {
+                System.out.println("   .-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-.");
+                System.out.println(".-'---      - ---     --     ---   - -    ---'-.");
+                System.out.println(" )                                            (");
+                System.out.println("(                                              )");
+                System.out.println(" )                                            (");
+                System.out.println("(              Cual idioma se usa?             )");
+                System.out.println(" )        (Poner espanol si es el caso)       (");
+                System.out.println("(                                              )");
+                System.out.println(" )                                            (");
+                System.out.println("(___       _       _       _       _        ___)");
+                System.out.println("    (_____) (_____) (_____) (_____)  (_____)");
+                System.out.println("            (_____) (_____) (_____)");
+                System.out.println("                    (_____)                  ");
+        }
+        public static void continente() {
+                System.out.println("   .-=-._.-=-._.-=-._.-=-._.-=-._.-=-._.-=-.");
+                System.out.println(".-'---      - ---     --     ---   - -    ---'-.");
+                System.out.println(" )                                            (");
+                System.out.println("(                                              )");
+                System.out.println(" )                                            (");
+                System.out.println("(       En que continente se encuentra?        )");
+                System.out.println(" )                                            (");
+                System.out.println("(                                              )");
+                System.out.println(" )                                            (");
+                System.out.println("(___       _       _       _       _        ___)");
+                System.out.println("    (_____) (_____) (_____) (_____)  (_____)");
+                System.out.println("            (_____) (_____) (_____)");
+                System.out.println("                    (_____)                  ");
+        }
 }

0c122f3cd472cc56b2b74b8389469c5436b3eee9
Juan Pablo Montoya Orozco <juan.montoyao@upb.edu.co>
2020-10-18 13:33:16 -0500

    Archivo con diseños ascii

---
+++ Ascii.java
@@ -0,0 +1,41 @@
+public class Ascii{
+	public static void acertaste() {
+                System.out.println("   \\  :  /       \\  :  /       \\ :  /       \\  :  /");
+                System.out.println("`. __/ \\__ .' `. __/ \\__ .'`. __/ \\__ .' `. __/ \\__ .'");
+                System.out.println("_ _\\     /_ _ _ _\\     /_ __ _\\     /_ _ _ _\\     /_ _");
+                System.out.println("   /_   _\\       /_   _\\      /_   _\\       /_   _\\");
+                System.out.println(" .'  \\ /  `.   .'  \\ /  `.  .'  \\ /  `.   .'  \\ /  `.");
+                System.out.println("   /  |  \\       /  :  \\      /  :  \\       /  |  \\");
+                System.out.println("      |                                        |");
+                System.out.println("   \\  |  /                                  \\  |  /");
+                System.out.println("`. __/ \\__ .'                            `. __/ \\__ .'");
+                System.out.println("_ _\\     /_ _          Acertaste         _ _\\     /_ _");
+                System.out.println("   /_   _\\                                  /_   _\\");
+                System.out.println(" .'  \\ /  `.                              .'  \\ /  `.");
+                System.out.println("   /  |  \\                                  /  |  \\");
+                System.out.println("      |                                        |");
+                System.out.println("   \\  |  /       \\  :  /      \\  :  /       \\  |  /");
+                System.out.println("`. __/ \\__ .' `. __/ \\__ .'`. __/ \\__ .' `. __/ \\__ .'");
+                System.out.println("_ _\\     /_ _ _ _\\     /_ __ _\\     /_ _ _ _\\     /_ _");
+                System.out.println("   /_   _\\       /_   _\\      /_   _\\       /_   _\\");
+                System.out.println(" .'  \\ /  `.   .'  \\ /  `.  .'  \\ /  `.   .'  \\ /  `.");
+                System.out.println("   /  :  \\       /  :  \\      /  :  \\      /  :  \\");
+        }
+        public static void fallaste() {
+                System.out.println("( }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ )");
+                System.out.println(" X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X");
+                System.out.println("(_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_)");
+                System.out.println(" X                                            X");
+                System.out.println(" X                                            X");
+                System.out.println(" X                                            X");
+                System.out.println(" X                                            X");
+                System.out.println(" X               No Acertaste :(              X");
+                System.out.println(" X                                            X");
+                System.out.println(" X                                            X");
+                System.out.println(" X                                            X");
+                System.out.println(" X                                            X");
+                System.out.println("( }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ }{ )");
+                System.out.println(" X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X");
+                System.out.println("(_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_}{_)");
+        }
+}

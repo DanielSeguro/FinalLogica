75ed525a70517b56d0a885e9a1b210482079f604
Daniel Rios <danielseguro@gmail.com>
2020-10-19 16:25:47 -0500

    Se crea el java que contiene el modo de juego 2

---
+++ adivinapais.java
@@ -0,0 +1,235 @@
+
+public class adivinapais {
+    public static void chile() {
+        Banderas.chile();
+        Ascii.adivinapais();
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ )  1. Chile    ( / )");
+        System.out.println(" ( ) (  2. Peru     ) ( )");
+        System.out.println(" ( / )  3. Polonia  ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 1) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Chile");
+        }
+    }
+
+    public static void colombia() {
+        Banderas.colombia();
+        Ascii.adivinapais();
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ ) 1. Venezuela ( / )");
+        System.out.println(" ( ) ( 2. Colombia  ) ( )");
+        System.out.println(" ( / ) 3. Ecuador   ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 2) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Colombia");
+        }
+    }
+
+    public static void polonia() {
+        Banderas.polonia();
+        Ascii.adivinapais();
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ ) 1. Polonia   ( / )");
+        System.out.println(" ( ) ( 2. Colombia  ) ( )");
+        System.out.println(" ( / ) 3. Ecuador   ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 1) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Polonia");
+        }
+    }
+
+    public static void gran_Breta() {
+        Banderas.gran_Breta();
+        Ascii.adivinapais();
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ )1. GranBretana( / )");
+        System.out.println(" ( ) (2. Argentina  ) ( )");
+        System.out.println(" ( / )3. Chile      ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 1) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Gran Bretana");
+        }
+    }
+
+    public static void indonesia() {
+        Banderas.indonesia();
+        Ascii.adivinapais();
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ )1. Indonesia  ( / )");
+        System.out.println(" ( ) (2. Polonia    ) ( )");
+        System.out.println(" ( / )3. Colombia   ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 1) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Indonesia");
+        }
+    }
+
+    public static void peru() {
+        Banderas.peru();
+        Ascii.adivinapais();
+
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ )1. Italia     ( / )");
+        System.out.println(" ( ) (2. Alemania   ) ( )");
+        System.out.println(" ( / )3. Peru       ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 3) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Peru");
+        }
+    }
+
+    public static void alemania() {
+        Banderas.alemania();
+        Ascii.adivinapais();
+
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ )1. Nigeria    ( / )");
+        System.out.println(" ( ) (2. Polonia    ) ( )");
+        System.out.println(" ( / )3. Alemania   ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 3) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Alemania");
+        }
+    }
+
+    public static void noruega() {
+        Banderas.noruega();
+        Ascii.adivinapais();
+
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ )1. Uruguay    ( / )");
+        System.out.println(" ( ) (2. Noruega    ) ( )");
+        System.out.println(" ( / )3. Cuba       ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 2) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Noruega");
+        }
+    }
+
+    public static void cuba() {
+        Banderas.cuba();
+        Ascii.adivinapais();
+
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ )1. Uruguay    ( / )");
+        System.out.println(" ( ) (2. Noruega    ) ( )");
+        System.out.println(" ( / )3. Cuba       ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 3) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Cuba");
+        }
+    }
+
+    public static void romania() {
+        Banderas.romania();
+        Ascii.adivinapais();
+
+        System.out.println("   ____________________");
+        System.out.println("  (                    )");
+        System.out.println("  ( \\                / )");
+        System.out.println(" ( \\ )1. Romania    ( / )");
+        System.out.println(" ( ) (2. IslaPascua ) ( )");
+        System.out.println(" ( / )3. Peru       ( \\ )");
+        System.out.println("  ( /                \\ )");
+        System.out.println("   (__________________)");
+
+        System.out.print("Su respuesta es (numero): ");
+        int opcion = ConsoleInput.getInt();
+
+        if (opcion == 1) {
+            Ascii.acertaste();
+        } else {
+            Ascii.fallaste();
+            System.out.println("Respuesta correcta: Romania");
+        }
+    }
+
+}

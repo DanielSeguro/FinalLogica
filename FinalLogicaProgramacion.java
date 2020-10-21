69464470e4eee1330c409225840d993f7f37ecf0
Daniel Rios <danielseguro@gmail.com>
2020-10-21 12:48:41 -0500

    Se ensambla el juego en el java principal

--- FinalLogicaProgramacion.java
+++ FinalLogicaProgramacion.java
@@ -4,10 +4,27 @@ package finallogicaprogramacion;
 public class FinalLogicaProgramacion {
 
         public static void main(String[] args) {
-                
+                int i = 0;
+                do {
+               Ascii.menuprincipal();
+               i=ConsoleInput.getInt();
+               if (i==1){
+                       ModosDeJuego.juego1();
+               }
+               else if (i==2){
+                       adivinapais.adivina();
+               }
+               else if (i==3){
+               ModosDeJuego.juego3();
+        }
+        else if (i==4){
+                Ascii.agradecer();
+         }
+                }while (i !=4);
+        }    
         }
 
-}
+
 
   
 
